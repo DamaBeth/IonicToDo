69bfeafe48db058b0d06cb3cb5a173aaa9524bff
DamaBeth <44103927+DamaBeth@users.noreply.github.com>
2021-08-19 11:10:30 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
+    { title: 'Mi día', url: '/folder/Mi día', icon: 'mail' },
+    { title: 'Importante', url: '/folder/Importante', icon: 'paper-plane' },
+    { title: 'Planeado', url: '/folder/Planeado', icon: 'heart' },
+    { title: 'Tareas', url: '/folder/Tareas', icon: 'archive' },
+    { title: 'Clases virtuales', url: '/folder/Clases Virtuales', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
